93a22350b7ef5f8c3015f748280912e7d33223ac
Freeman Jiang <freeman.jiang.ca@gmail.com>
2021-07-29 13:23:12 -0400

    why

--- testing_is_necessary.js
+++ testing_is_necessary.js
@@ -3,21 +3,15 @@ const funAnimals = []
 funAnimals.push({
     name: "Hygge",
     type: "cat",
-    reason: "agressive snuggler"
+    why: "agressive snuggler"
 })
 
 funAnimals.push({
     name: "Trecito",
     type: "dog",
-    reason: "four legs worth of energy at the cost of three"
-})
-
-funAnimals.push({
-    name: "Peter",
-    type: "tortoise",
-    reason: "back yard grass eater"
+    why: "four legs worth of energy at the cost of three"
 })
 
 for (let animal of funAnimals) {
-    console.log(`${animal.name} the ${animal.type} is fun.  Reason: ${animal.reason}`)
+    console.log(`${animal.name} the ${animal.type} is fun. Why you ask? ${animal.why}`)
 }
